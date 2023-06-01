ed4941ab15e0b2319c5ff923e6396c1ca5eac4b0
vpi <vytautaspikelis@hotmail.com>
2023-06-01 16:41:38 +0300

    added requirements.txt, updated python

--- nvd-api.py
+++ nvd-api.py
@@ -1,15 +1,20 @@
 from flask import Flask, jsonify, request
 import nvdlib
 from flask_cors import CORS
+from termcolor import colored
+import colorama
+colorama.init()
 
 app = Flask(__name__)
 CORS(app)  # Apply CORS to your app
 
+OSUM = text2art("OSUM?")
+OSUM = colored(OSUM, "green")
+print(OSUM)
 
 @app.route('/api/v1/cpe_search', methods=['GET'])
 def cpe_search():
-    keyword = request.args.get(
-        'keyword', type=str)
+    keyword = request.args.get('keyword', type=str)
     limit = request.args.get('limit', type=int)
 
     r = nvdlib.searchCPE(keywordSearch=keyword, limit=limit)
@@ -19,15 +24,11 @@ def cpe_search():
     for eachCPE in r:
         cpe_data = {
             'cpeName': eachCPE.cpeName,
-            }
+        }
         results.append(cpe_data)
 
     return jsonify(results)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-# https://nvdlib.com/en/latest/
- 
+    app.run(debug=False, host='127.0.0.1', port=5000, threaded=True)
