5066c2d2481a373ae34ed348b500f3f7776916e1
vycioha <44521393+vycioha@users.noreply.github.com>
2023-06-21 00:48:14 +0300

    solving conflicts

--- static/js/darkModeToggle.js
+++ static/js/darkModeToggle.js
@@ -2,35 +2,34 @@
 function toggleDarkMode() {
     var darkModeButton = document.querySelector('#darkModeButton');
     var isDarkMode = document.body.classList.contains('dark-mode');
-  
+
     // Toggle dark mode class
     document.body.classList.toggle('dark-mode');
-  
+
     // Toggle the icon
     if (isDarkMode) {
-      darkModeButton.classList.remove('fa-sun');
-      darkModeButton.classList.add('fa-moon');
+        darkModeButton.classList.remove('fa-sun');
+        darkModeButton.classList.add('fa-moon');
     } else {
-      darkModeButton.classList.remove('fa-moon');
-      darkModeButton.classList.add('fa-sun');
+        darkModeButton.classList.remove('fa-moon');
+        darkModeButton.classList.add('fa-sun');
     }
-  
+
     // Update dark mode status in localStorage
     if (document.body.classList.contains('dark-mode')) {
-      localStorage.setItem('darkMode', 'enabled');
+        localStorage.setItem('darkMode', 'enabled');
     } else {
-      localStorage.setItem('darkMode', 'disabled');
+        localStorage.setItem('darkMode', 'disabled');
     }
-  }
-  
-  // Event listener for dark mode button
-  document.querySelector('#darkModeButton').addEventListener('click', toggleDarkMode);
-  
-  // On page load, check if dark mode is enabled in localStorage
-  if (localStorage.getItem('darkMode') === 'enabled') {
+}
+
+// Event listener for dark mode button
+document.querySelector('#darkModeButton').addEventListener('click', toggleDarkMode);
+
+// On page load, check if dark mode is enabled in localStorage
+if (localStorage.getItem('darkMode') === 'enabled') {
     document.body.classList.add('dark-mode');
     var darkModeButton = document.querySelector('#darkModeButton');
     darkModeButton.classList.remove('fa-moon');
     darkModeButton.classList.add('fa-sun');
-  }
-  
+}
--- static/js/formSubmission.js
+++ static/js/formSubmission.js
@@ -1,48 +1,48 @@
-document.getElementById("software-form").addEventListener("submit", function(e) {
-    e.preventDefault();
+document
+    .getElementById("software-form")
+    .addEventListener("submit", function(e) {
+        e.preventDefault();
 
-    let softwareInput = document.getElementById("software-input").value;
+        let softwareInput = document.getElementById("software-input").value;
 
-    document.getElementById("loading-animations").style.display = "block";
+        document.getElementById("loading-animations").style.display = "block";
 
+        // Call your backend with the software name, update the CPE dropdown when data returns
+        fetch("http://localhost:5000/api/v1/cpe_search?keyword=" + softwareInput)
+            .then((response) => response.json())
+            .then((data) => {
+                let select = document.getElementById("cpe-select");
+                // Remove old options
+                select.innerHTML = "";
 
+                // Check if data is empty
+                if (data.length === 0) {
+                    // No matching CPEs were found
+                    let errorMessage = document.getElementById("error-message");
+                    errorMessage.textContent = "No matching CPEs were found.";
+                    errorMessage.style.display = "block";
+                    document.getElementById("cpe-picker").style.display = "none";
+                    document.getElementById("table-container").style.display = "none";
+                } else {
+                    // Add new options
+                    data.forEach((cpe) => {
+                        console.log(cpe);
+                        let option = document.createElement("option");
+                        option.text = cpe.cpeName;
+                        option.value = cpe.cpeName; // Assuming you want to use the CPE name as the value
+                        select.add(option);
+                    });
+                    // Hide the error message if previously shown
+                    let errorMessage = document.getElementById("error-message");
+                    errorMessage.style.display = "none";
+                    document.getElementById("cpe-picker").style.display = "block";
+                }
 
-    // Call your backend with the software name, update the CPE dropdown when data returns
-    fetch("http://localhost:5000/api/v1/cpe_search?keyword=" + softwareInput)
-        .then((response) => response.json())
-        .then((data) => {
-            let select = document.getElementById("cpe-select");
-            // Remove old options
-            select.innerHTML = "";
-
-            // Check if data is empty
-            if (data.length === 0) {
-                // No matching CPEs were found
-                let errorMessage = document.getElementById("error-message");
-                errorMessage.textContent = "No matching CPEs were found.";
-                errorMessage.style.display = "block";
-                document.getElementById("cpe-picker").style.display = "none";
-                document.getElementById("table-container").style.display = "none";
-            } else {
-                // Add new options
-                data.forEach((cpe) => {
-                    console.log(cpe);
-                    let option = document.createElement("option");
-                    option.text = cpe.cpeName;
-                    option.value = cpe.cpeName; // Assuming you want to use the CPE name as the value
-                    select.add(option);
-                });
-                // Hide the error message if previously shown
-                let errorMessage = document.getElementById("error-message");
-                errorMessage.style.display = "none";
-                document.getElementById("cpe-picker").style.display = "block";
-            }
-
-            // Hide the loading message after data is loaded
-            document.getElementById("loading-animations").style.display = "none";
-        })
-        .catch((error) => {
-            // Handle any errors during the fetch operation
-            console.error("Error:", error);
-        });
-});
+                // Hide the loading message after data is loaded
+                document.getElementById("loading-animations").style.display = "none";
+            })
+            .catch((error) => {
+                // Handle any errors during the fetch operation
+                console.error("Error:", error);
+            });
+    });
